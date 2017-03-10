1543160a0addf48a6720f2de6dd5792ba8fd3729
Simon Hildrew <simon.hildrew@theguardian.com>
2017-03-10 00:35:56 +0000

    Add a marginally useful comment

--- src/main/scala/configrule/cfn/extractors/ResourceType.scala
+++ src/main/scala/configrule/cfn/extractors/ResourceType.scala
@@ -4,6 +4,7 @@ trait ResourceType[T] {
   def awsType: String
   def name(t: T): String
   def fetchAll: List[T]
+  /** is a particular instance not applicable */
   def notApplicable: Set[String] = Set.empty
   def isNotApplicable(t: T) = notApplicable.contains(name(t))
 }
